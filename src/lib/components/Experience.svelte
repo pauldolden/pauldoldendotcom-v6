b7eecc6a66ae87449e93ae1e7785e68eddb77be8
Paul Dolden <paul@dolden.co.uk>
2024-05-07 20:48:07 +0100

    experience

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -6,10 +6,8 @@
 	{#each jobs as job}
 		<div class="py-2">
 			<div class="flex flex-col gap-1">
-				<p class="text-pRed font-bold">{job.company}</p>
-				<p class="text-tmux">{job.title}</p>
+				<p class="text-tmux font-bold">{job.title} -> {job.company}</p>
 				<p>{job.duration}</p>
-				<p>{job.location}</p>
 				<p>{job.description}</p>
 			</div>
 		</div>
