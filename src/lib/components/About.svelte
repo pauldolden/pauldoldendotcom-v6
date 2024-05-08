15c1101f7b99ed0e2da2777e7731bae66741a4f7
Paul Dolden <paul@dolden.co.uk>
2024-05-08 07:25:13 +0100

    target blank on links

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,22 +1,24 @@
 <div class="opacity-90 flex flex-col gap-1">
 	<p>
 		Hi, I'm Paul Dolden. I'm a developer based in the East of England and currently work as a Senior
-		Software Engineer at <a href="https://thefa.com" class="underline text-tmux"
+		Software Engineer at <a href="https://thefa.com" class="underline text-tmux" target="_blank"
 			>The Football Association</a
 		>, building applications to help England teams succeed.
 	</p>
 	<p>I love writing code and enjoy experimenting with new tools and technologies.</p>
 	<p>
 		Currently, I'm working with:
-		<a href="https://svelte.dev/" class="underline text-tmux">Svelte</a>,
-		<a href="https://www.typescriptlang.org/" class="underline text-tmux">TypeScript</a>,
-		<a href="https://go.dev/" class="underline text-tmux">Go</a>, and
-		<a href="https://tailwindcss.com/" class="underline text-tmux">TailwindCSS</a>.
+		<a href="https://svelte.dev/" class="underline text-tmux" target="_blank">Svelte</a>,
+		<a href="https://www.typescriptlang.org/" class="underline text-tmux" target="_blank"
+			>TypeScript</a
+		>,
+		<a href="https://go.dev/" class="underline text-tmux" target="_blank">Go</a>, and
+		<a href="https://tailwindcss.com/" class="underline text-tmux" target="_blank">TailwindCSS</a>.
 	</p>
 	<p>
 		I have a particular passion for building for the cloud and am a big fan of the serverless
 		paradigm. I also have a keen interest in the world of
-		<a href="https://dora.dev" class="underline text-tmux">DevOps</a>
+		<a href="https://dora.dev" class="underline text-tmux" target="_blank">DevOps</a>
 		and CI/CD.
 	</p>
 	<p>
@@ -25,7 +27,10 @@
 	</p>
 	<p>
 		If you'd like to get in touch, feel free to shoot me an email
-		<a href="mailto:paul@dolden.co.uk" class="underline text-tmux">here</a> or connect with me on
-		<a href="https://linkedin.com/in/pauldolden" class="underline text-tmux">LinkedIn</a>.
+		<a href="mailto:paul@dolden.co.uk" class="underline text-tmux" target="_blank">here</a> or
+		connect with me on
+		<a href="https://linkedin.com/in/pauldolden" class="underline text-tmux" target="_blank"
+			>LinkedIn</a
+		>.
 	</p>
 </div>
