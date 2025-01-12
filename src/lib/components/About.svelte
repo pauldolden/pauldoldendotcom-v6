78e93c88f6dad6abf24fbe48211fd2922d4efa83
Paul Dolden <paul@dolden.co.uk>
2025-01-12 11:54:06 +0000

    feat: updates

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,32 +1,36 @@
 <div class="opacity-90 flex flex-col gap-1">
 	<p>
-		Hi, I'm Paul Dolden. I'm a developer based in the East of England and currently work as a Senior
+		Hi, I'm Paul Dolden. I'm a developer based in the East of England, currently working as a Senior
 		Software Engineer at <a href="https://thefa.com" class="underline text-tmux" target="_blank"
 			>The Football Association</a
-		>, building applications to help England teams succeed.
+		>, building applications that help England teams succeed.
 	</p>
-	<p>I love writing code and enjoy experimenting with new tools and technologies.</p>
+	<p>I enjoy writing code and experimenting with new tools and technologies.</p>
 	<p>
-		Currently, I'm working with:
+		Right now, I'm working with:
 		<a href="https://svelte.dev/" class="underline text-tmux" target="_blank">Svelte</a>,
 		<a href="https://www.typescriptlang.org/" class="underline text-tmux" target="_blank"
 			>TypeScript</a
 		>,
 		<a href="https://go.dev/" class="underline text-tmux" target="_blank">Go</a>, and
 		<a href="https://tailwindcss.com/" class="underline text-tmux" target="_blank">TailwindCSS</a>.
+		I'm also exploring
+		<a href="https://www.rust-lang.org/" class="underline text-tmux" target="_blank">Rust</a>
+		and
+		<a href="https://webassembly.org/" class="underline text-tmux" target="_blank">WebAssembly</a>.
 	</p>
 	<p>
-		I have a particular passion for building for the cloud and am a big fan of the serverless
-		paradigm. I also have a keen interest in the world of
+		I enjoy building for the cloud, working with serverless technologies, containers, and
+		orchestration. I'm also interested in
 		<a href="https://dora.dev" class="underline text-tmux" target="_blank">DevOps</a>
-		and CI/CD.
+		and ensuring our applications run on reliable and scalable infrastructure.
 	</p>
 	<p>
-		When I'm not coding, I enjoy spending time with my family, watching football, and, much less
-		frequently, playing video games.
+		When I'm not coding, I enjoy hanging out with my wife and kids. I also love escape rooms,
+		cooking, watching football, and (very rarely) playing video games.
 	</p>
 	<p>
-		If you'd like to get in touch, feel free to shoot me an email
+		If you'd like to get in touch, feel free to send me an email
 		<a href="mailto:paul@dolden.co.uk" class="underline text-tmux" target="_blank">here</a> or
 		connect with me on
 		<a href="https://linkedin.com/in/pauldolden" class="underline text-tmux" target="_blank"
--- src/lib/data/jobs.ts
+++ src/lib/data/jobs.ts
@@ -15,63 +15,65 @@ type Tool = {
 
 export const jobs: Job[] = [
 	{
-		title: 'Senior Full-Stack Developer',
-		company: 'The Football Association',
-		location: 'Wembley Stadium, London',
-		duration: 'Aug 2023 - Present',
-		description: 'Working for England teams',
-		url: 'https://www.thefa.com',
-		tools: [
-			{ name: 'React', url: 'https://reactjs.org' },
-			{ name: 'Node.js', url: 'https://nodejs.org' },
-			{ name: 'TypeScript', url: 'https://www.typescriptlang.org' },
-			{ name: 'GCP', url: 'https://cloud.google.com' }
-		]
-	},
-	{
-		title: 'Full-Stack Developer',
-		company: 'Comic Relief',
-		location: 'London',
-		duration: 'May 2022 - Aug 2023',
+		title: "Senior Full-Stack Developer",
+		company: "The Football Association",
+		location: "Wembley Stadium, London",
+		duration: "Aug 2023 - Present",
 		description:
-			'As a Full-Stack Engineer, I played a key role in maintaining and enhancing our React front-end and Node.js back-end services. Alongside a collaborative team, we efficiently managed our infrastructure using AWS CloudFormation and the Serverless framework. Testing was a priority, so we utilized Cypress, Jest, and Mocha to ensure robustness, even during peak campaigns like Red Nose Day. Collaboration was vital—I closely engaged with stakeholders and team members to keep our digital fundraising projects on track. Additionally, I contributed to the transition from JavaScript to TypeScript, enhancing our codebase for long-term sustainability.',
-		url: 'https://www.comicrelief.com',
+			"Building software that helps England teams win. My focus is on driving engineering excellence, championing modern tools, technologies, and practices to tackle our unique challenges in the most innovative and effective ways.",
+		url: "https://www.thefa.com",
 		tools: [
-			{ name: 'React', url: 'https://reactjs.org' },
-			{ name: 'Node.js', url: 'https://nodejs.org' },
-			{ name: 'TypeScript', url: 'https://www.typescriptlang.org' },
-			{ name: 'AWS', url: 'https://aws.amazon.com' },
-			{ name: 'Serverless Framework', url: 'https://www.serverless.com' }
-		]
+			{ name: "React", url: "https://reactjs.org" },
+			{ name: "Node.js", url: "https://nodejs.org" },
+			{ name: "TypeScript", url: "https://www.typescriptlang.org" },
+			{ name: "Terraform", url: "https://www.terraform.io" },
+			{ name: "GCP", url: "https://cloud.google.com" },
+		],
 	},
 	{
-		title: 'Lead Mobile Developer',
-		company: '2D Media',
-		duration: 'March 2021 - May 2022',
-		location: 'Witham',
+		title: "Full-Stack Developer",
+		company: "Comic Relief",
+		location: "London",
+		duration: "May 2022 - Aug 2023",
+		description:
+			"As a Full-Stack Engineer, I played a key role in maintaining and enhancing our React front-end and Node.js back-end services. Alongside a collaborative team, we efficiently managed our infrastructure using AWS CloudFormation and the Serverless framework. Testing was a priority, so we utilized Cypress, Jest, and Mocha to ensure robustness, even during peak campaigns like Red Nose Day. Collaboration was vital—I closely engaged with stakeholders and team members to keep our digital fundraising projects on track. Additionally, I contributed to the transition from JavaScript to TypeScript, enhancing our codebase for long-term sustainability.",
+		url: "https://www.comicrelief.com",
+		tools: [
+			{ name: "React", url: "https://reactjs.org" },
+			{ name: "Node.js", url: "https://nodejs.org" },
+			{ name: "TypeScript", url: "https://www.typescriptlang.org" },
+			{ name: "AWS", url: "https://aws.amazon.com" },
+			{ name: "Serverless Framework", url: "https://www.serverless.com" },
+		],
+	},
+	{
+		title: "Lead Mobile Developer",
+		company: "2D Media",
+		duration: "March 2021 - May 2022",
+		location: "Witham",
 		description:
 			"I spearheaded our company's venture into mobile app development using React Native, advocating for modern tooling and frameworks to enhance code consistency and quality. Among my projects was the creation of custom mobile apps, including a social network tailored for the health and social care sector. Additionally, I played a role in adapting internal tools such as the CRM, project management software, and e-commerce codebase into native mobile applications. Managing app distribution through the Play Store and App Store was also part of my responsibilities. Throughout these endeavors, I maintained close communication with stakeholders to ensure the delivery of top-notch products.",
-		url: 'https://www.2dmedia.co.uk',
+		url: "https://www.2dmedia.co.uk",
 		tools: [
-			{ name: 'React Native', url: 'https://reactnative.dev' },
-			{ name: 'Node.js', url: 'https://nodejs.org' },
-			{ name: 'TypeScript', url: 'https://www.typescriptlang.org' }
-		]
+			{ name: "React Native", url: "https://reactnative.dev" },
+			{ name: "Node.js", url: "https://nodejs.org" },
+			{ name: "TypeScript", url: "https://www.typescriptlang.org" },
+		],
 	},
 	{
-		title: 'Web Developer',
-		company: '2D Media',
-		duration: 'Jan 2021 - March 2021',
-		location: 'Witham',
+		title: "Web Developer",
+		company: "2D Media",
+		duration: "Jan 2021 - March 2021",
+		location: "Witham",
 		description:
-			'I utilized our internal CMS and platform to develop and maintain e-commerce websites. These platforms were tailored to our specific needs and operated similarly to popular e-commerce solutions. I was responsible for setting up and optimizing these sites for performance and user experience, leveraging our internal tools and systems for seamless integration and functionality. By leveraging our internal resources, I created customized e-commerce solutions that effectively met our business requirements and provided a competitive edge in the market.',
-		url: 'https://www.2dmedia.co.uk',
+			"I utilized our internal CMS and platform to develop and maintain e-commerce websites. These platforms were tailored to our specific needs and operated similarly to popular e-commerce solutions. I was responsible for setting up and optimizing these sites for performance and user experience, leveraging our internal tools and systems for seamless integration and functionality. By leveraging our internal resources, I created customized e-commerce solutions that effectively met our business requirements and provided a competitive edge in the market.",
+		url: "https://www.2dmedia.co.uk",
 		tools: [
-			{ name: 'React', url: 'https://reactjs.org' },
-			{ name: 'Node.js', url: 'https://nodejs.org' },
-			{ name: 'TypeScript', url: 'https://www.typescriptlang.org' },
-			{ name: 'Tailwind CSS', url: 'https://tailwindcss.com' },
-			{ name: 'PHP', url: 'https://www.php.net' }
-		]
-	}
+			{ name: "React", url: "https://reactjs.org" },
+			{ name: "Node.js", url: "https://nodejs.org" },
+			{ name: "TypeScript", url: "https://www.typescriptlang.org" },
+			{ name: "Tailwind CSS", url: "https://tailwindcss.com" },
+			{ name: "PHP", url: "https://www.php.net" },
+		],
+	},
 ];
